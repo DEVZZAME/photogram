0da9592c704b9d868f77e69c6a40e17426851583
DEVZZAME <dev.zzame@gmail.com>
2022-03-03 21:45:16 +0900

    Likes

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,19 +1,25 @@
 package com.cos.photogramstart.domain.image;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+import javax.persistence.Transient;
 
 import org.hibernate.annotations.ManyToAny;
 
+import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.subscribe.Subscribe;
 import com.cos.photogramstart.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,25 +32,35 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 public class Image {
-	//연관관계 : 한명의 유저는 여러개의 사진을 등록 가능하지만, 하나의 이미지를 여러명이 등록은 불가하기 때문에
-	//N : 1의 관계가 됨. ManyToOne
+	// 연관관계 : 한명의 유저는 여러개의 사진을 등록 가능하지만, 하나의 이미지를 여러명이 등록은 불가하기 때문에
+	// N : 1의 관계가 됨. ManyToOne
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String caption; // 사진에 관한 설명
 	private String postImageUrl; // 사진을 전송 받아서 그 사진을 서버의 특정 폴더에 저장 - DB에 그 저장된 경로를 INSERT
-	
+
+	@JsonIgnoreProperties({ "images" })
 	@JoinColumn(name = "userId")
-	@ManyToOne
-	private User user; // 어떤 유저가 업로드 했는지 알아야 하니까...
+	@ManyToOne(fetch = FetchType.EAGER) // 이미지를 select하면 조인해서 User정보를 같이 들고옴
+	private User user; // 1, 1
 
-	//이미지 좋아요 기능
-	
-	//댓글 기능
-	
-	private LocalDateTime createDate; //DB에는 항상 시간이 필요함. 언제 DB가 들어왔는지 알아야 하니까...
+	// 이미지 좋아요
+	@JsonIgnoreProperties({ "image" })
+	@OneToMany(mappedBy = "image")
+	private List<Likes> likes;
 
+	// 댓글 기능
+
+	private LocalDateTime createDate; // DB에는 항상 시간이 필요함. 언제 DB가 들어왔는지 알아야 하니까...
+
+	@Transient // DB에 컬럼이 만들어지지 않는다.
+	private boolean likeState;
+
+	@Transient
+	private int likeCount;
+	
 	@PrePersist
 	public void createDate() {
 		this.createDate = LocalDateTime.now();
@@ -56,7 +72,5 @@ public class Image {
 //		return "Image [id=" + id + ", caption=" + caption + ", postImageUrl=" + postImageUrl 
 //				+ ", createDate=" + createDate + "]";
 //	}
-	
-	
-	
+
 }
--- src/main/webapp/WEB-INF/views/user/profile.jsp
+++ src/main/webapp/WEB-INF/views/user/profile.jsp
@@ -56,7 +56,7 @@
 				<ul>
 					<li><a href=""> 게시물<span>${dto.imageCount}</span>
 					</a></li>
-					<li><a href="javascript:subscribeInfoModalOpen();"> 구독정보<span>${dto.subscribeCount}</span>
+					<li><a href="javascript:subscribeInfoModalOpen(${dto.user.id});"> 구독정보<span>${dto.subscribeCount}</span>
 					</a></li>
 				</ul>
 			</div>
@@ -129,9 +129,10 @@
 				<i class="fas fa-times"></i>
 			</button>
 		</div>
-		
-		
-			
+
+		<div class="subscribe-list" id="subscribeModalList">
+
+
 			
 		</div>
 	</div>
