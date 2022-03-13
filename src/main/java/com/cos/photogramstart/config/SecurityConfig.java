59ecb4d91b15b9cc7ccda70fb290af8b0d162fbe
zzame <dev.zzame@gmail.com>
2022-03-13 21:56:56 +0900

    Photogram final version

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -20,8 +20,8 @@ import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@EnableWebSecurity // 해당 파일로 시큐리티를 활성화
-@Configuration // IoC     
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private final OAuth2DetailsService oAuth2DetailsService;
@@ -33,19 +33,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨.
+
 		http.csrf().disable();
 		http.authorizeRequests()
 			.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**", "/api/**").authenticated()
 			.anyRequest().permitAll()
 			.and()
 			.formLogin()
-			.loginPage("/auth/signin") // GET
-			.loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행
+			.loginPage("/auth/signin")
+			.loginProcessingUrl("/auth/signin")
 			.defaultSuccessUrl("/")
 			.and()
-			.oauth2Login() // form로그인도 하는데, oauth2로그인도 할꺼야!!
-			.userInfoEndpoint() // oauth2로그인을 하면 최종응답을 회원정보를 바로 받을 수 있다.
+			.oauth2Login()
+			.userInfoEndpoint()
 			.userService(oAuth2DetailsService);
 	}
 	
